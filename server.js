25fa9becec0e0f5f09cf7e1305377bbe6efc97aa
Priyank Kr. Nigam <priyankkrnigam@gmail.com>
2025-05-08 20:48:26 +0530

    server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000; // ✅ Use dynamic port for Render
+const PORT = process.env.PORT || 3000;
 
 const users = {}; // socket.id → username
 
